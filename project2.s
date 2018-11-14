daf8a12ac3fe33769fa577f8b0cea1f0237d8240
Latifat <latifat.ozoya@bison.howard.edu>
2018-11-14 13:22:19 -0500

    users can input a string or text

--- project2.s
+++ project2.s
@@ -3,3 +3,8 @@
 	too_large: .asciiz "Input is too long."
 	is_empty: .asciiz "Input is empty."
 	is_invalid: .asciiz "Invalid base-34 number."
+.text
+    main:
+	li $v0, 8   #used to get user input as text and displays it
+	la $a0, user_input
+	li $a1, 100
