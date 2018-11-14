95f72ecf821aa7b490f97ce8a47c4260e16e51c6
Latifat <latifat.ozoya@bison.howard.edu>
2018-11-14 13:26:38 -0500

    user's input can be displayed due to added syscall. Also, to remove the extra spaces before a string this branch was made.

--- project2.s
+++ project2.s
@@ -5,6 +5,13 @@
 	is_invalid: .asciiz "Invalid base-34 number."
 .text
     main:
-	li $v0, 8   #used to get user input as text and displays it
+	li $v0, 8              #used to get user input as text and displays it
 	la $a0, user_input
 	li $a1, 100
+	syscall
+		
+	# Remove leading spaces
+	remove_space_before:
+	li $t9, 32                      # save space char to t9
+	lb $t5, 0($a0)                  # load first input char into t5
+	beq $t9, $t5, remove_first_char # remove the first char if it is a space
